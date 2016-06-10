b1aaa25099b0788114e47b256e5cb20442091a66
Brian <briancorrea@gmail.com>
2016-06-10 19:49:48 -0400

    Setting up sessions

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var sessions = require('client-sessions');
 
 var routes = require('./routes/index');
 var api = require('./routes/api');
+var account = require('./routes/account');
+
 var dbUrl = 'mongodb://localhost/Chat'
 mongoose.connect(dbUrl, function(err, res){
   if(err){
@@ -30,10 +33,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sessions({
+  cookieName: 'session',
+  secret: 'sdjdsfhdkfhj',
+  duration: 24*60*60*1000, //24 hours
+  activeDuration: 30*60*1000
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
+app.use('/account', account);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
