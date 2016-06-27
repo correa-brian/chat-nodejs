915bb166d39cac0141f0c4c2fa0d534a3740644a
Brian <briancorrea@gmail.com>
2016-06-27 12:25:24 -0400

    Renaming MongoDB

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/api', api);
-app.use('/account', account);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
